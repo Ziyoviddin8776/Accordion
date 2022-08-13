ede1af62f7bf7bf17533968c66b7ebc83185ec80
Ziyoviddin8776 <ziyoviddin8776@gmail.com>
2022-08-13 18:59:52 +0500

    Accordion And Menu

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import questions from "./data";
+import data from "./data";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h3>Question And Answers About Login</h3>
+        <section className="info">
+          {questions.map((question) => (
+            <SingleQuestion key={question.id} {...question}></SingleQuestion>
+          ))}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
